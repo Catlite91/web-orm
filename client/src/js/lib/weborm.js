fc3b31aff78abe9ebb5216123aa5a59e0716f2bf
zhuxinxin <zhuxinxin@xiaomei.com>
2015-12-04 00:49:26 +0800

    解析

--- client/src/js/lib/weborm.js
+++ client/src/js/lib/weborm.js
@@ -1,14 +1,37 @@
+var DATA_MAP = 'dataMap';
+var firstWord = /([^\.]+)\./;
 var _createService = function (config) {
-        var service = {a: 12};
-
+        var service = {};
+        var requireUrlMap = {},
+            item;
+        for (var key in config[DATA_MAP]) {
+            item = config[DATA_MAP][key];
+            requireUrlMap[key] = _getRequireUrl(item);
+        }
         return service;
     },
     /**
      * 获取所需的url
      * @private
      */
-    _getRequireUrl = function () {
-
+    _getRequireUrl = function (object) {
+        var obj = object,
+            value = [],
+            result = [],
+            _result = {},
+            _item;
+        // 遍历对象,获取所有值
+        getAllObjValue(obj, value);
+        // 去重
+        for (var i = 0, len = value.length; i < len; ++i) {
+            _item = value[i].match(firstWord)[1];
+            _result[_item] = true;
+        }
+        // Object -> Array
+        for (var key in _result) {
+            result.push(key);
+        }
+        return result;
     },
     /**
      * restful接口获取字段
@@ -19,6 +42,22 @@ var _createService = function (config) {
     _getData = function (url, property) {
 
     };
+
+/**
+ * 递归遍历对象
+ * @param object
+ * @param result
+ */
+function getAllObjValue(object, result) {
+    for (var key in object) {
+        var value = object[key];
+        if (typeof value === 'string') {
+            result.push(value);
+        } else if (typeof value === 'object') {
+            arguments.callee.call(this, value, result);
+        }
+    }
+}
 
 module.exports = {
     createService: _createService
